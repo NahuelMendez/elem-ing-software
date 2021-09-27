af32790abee2236d50c702aa1fae485e35eb3f60
emilianaAilen <emiliana.ailen@hotmail.com>
2021-09-27 02:02:30 -0300

    fix my menu load product, error and success messages added

--- app/src/components/MyMenu/index.js
+++ app/src/components/MyMenu/index.js
@@ -3,12 +3,13 @@ import api from '../../Api/ApiObject';
 import NavBar from "../Navbar";
 
 const MyMenu = () => {
-  const [producData, setProductData] = useState({
+  const [productData, setProductData] = useState({
     name: '',
     description: '',
     price: '',
-    imageUrl: ''
+    imageURL: ''
   })
+  const [result, setResult] = useState({ show: false, message: "", error: false })
 
   const handleChange = event => {
     const target = event.target
@@ -17,23 +18,26 @@ const MyMenu = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    api.addProduct([producData], "")
+    api.addProduct({ menu: [productData] }, "pizzeria")
       .then(_ => {
-        console.log("exito")
+        setResult({ show: true, message: "No se ha podido ingresar el producto", error: false })
       })
-      .catch(error => console.log(error.response.data.result))
+      .catch(() => {
+        setResult({ show: true, message: "No se ha podido ingresar el producto", error: true })
+      })
   }
 
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center mt-8">
-          <input placeholder="Nombre" required className="input mb-2" value={producData.name} name={"name"} type={"text"} onChange={handleChange} />
-          <input placeholder="Precio" required className="input mb-2" value={producData.price} name={"price"} type={"number"} onChange={handleChange} />
-          <input placeholder="Descripción" required className="input mb-2" value={producData.description} name={"description"} type={"text"} onChange={handleChange} />
-          <input placeholder="URL imágen" required className="input" value={producData.imageUrl} name={"imageUrl"} type={"text"} onChange={handleChange} />
-        <button className="mt-4 font-medium rounded-lg focus:outline-none p-2 bg-principal text-white" type="submit">Agregar producto</button>
+        <input placeholder="Nombre" required className="input mb-2" value={productData.name} name={"name"} type={"text"} onChange={handleChange} />
+        <input placeholder="Precio" required className="input mb-2" value={productData.price} name={"price"} type={"number"} onChange={handleChange} />
+        <input placeholder="Descripción" required className="input mb-2" value={productData.description} name={"description"} type={"text"} onChange={handleChange} />
+        <input placeholder="URL imágen" required className="input" value={productData.imageURL} name={"imageURL"} type={"text"} onChange={handleChange} />
+        <button className="mt-4 button-principal" type="submit">Agregar producto</button>
       </form>
+      {result.show && <p className={`text-center mt-4 ${result.error ? "text-red-500" : "text-green-500"}`}>{result.message}</p>}
     </div>
   )
 }
