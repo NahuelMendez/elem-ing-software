e0186daa91b7da617afeb2019a9dea08a546e8b7
David Eduardo Gonzalez <davidgonzalezjscosasunq@gmail.com>
2021-10-04 17:09:17 -0300

    Added end-to-end tests for pizzeria adding products

--- backend/e2e-test/pizzeria-menu-page.e2e.spec.js
+++ backend/e2e-test/pizzeria-menu-page.e2e.spec.js
@@ -2,18 +2,19 @@ const puppeteer = require('puppeteer')
 const {
     registerAsPizzeriaAndGoToMenu,
     addProduct,
-    expectTextContent
+    expectTextContent,
+    goto
 } = require('./helpers/helpers')
 
 const { createPizzeriaRegistrationData, createPizzaData } = require('../test/testObjects')
-
+jest.setTimeout(10000)
 describe('Pizzeria menu', () => {
     let browser
     let page
 
     beforeEach(async () => {
-        browser = await puppeteer.launch({headless: false})
-        //browser = await puppeteer.launch()
+        //browser = await puppeteer.launch({headless: false})
+        browser = await puppeteer.launch()
         page = await browser.newPage()
     })
 
@@ -21,7 +22,16 @@ describe('Pizzeria menu', () => {
         await browser.close()
     })
 
-    it('when an authenticated pizzeria add a new product to its menu, a notification message appears', async () => {
+    it(`a just created pizzeria has no products on it's menu`, async () => {
+        const pizzeriaData = createPizzeriaRegistrationData({})
+        await registerAsPizzeriaAndGoToMenu(page, pizzeriaData)
+
+        const productsData = await page.$$eval('.card-container', elements => elements)
+
+        expect(productsData).toHaveLength(0)
+    })
+
+    it(`when an authenticated pizzeria add a product to its menu, it appears on it's home page`, async () => {
         const pizzeriaData = createPizzeriaRegistrationData({})
         const pizzaData = createPizzaData({})
         pizzeriaData.name = 'pizzeria'
@@ -29,7 +39,16 @@ describe('Pizzeria menu', () => {
 
         await addProduct(page, pizzaData)
 
-        await expectTextContent(page, '[name="submited-product-message"]', 'Se ha ingresado el producto correctamente')
+        await goto(page, '/home')
+
+        const productsData = await page.$$eval('.card-container', elements => elements.map(element => element.innerHTML))
+
+        expect(productsData).toHaveLength(1)
+
+        expect(productsData[0]).toContain(pizzaData.name)
+        expect(productsData[0]).toContain(pizzaData.description)
+        expect(productsData[0]).toContain(pizzaData.price)
+        expect(productsData[0]).toContain(pizzaData.imageURL)
     })
 
 })
