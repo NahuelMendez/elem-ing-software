ed96bbf2e28fc31ceab5bf6f49ce02b73bfe885c
davidgonzalezjscosasunq@gmail.com <davidgonzalezjscosasunq@gmail.com>
2021-10-08 20:46:44 -0300

    Refactor API removeProduct.test.js. Removed repeated code

--- backend/test/api/removeProduct.test.js
+++ backend/test/api/removeProduct.test.js
@@ -1,12 +1,18 @@
 const request = require('supertest')
 const {createApp} = require('../../src/api/app')
-const {registerPath, menuPath} = require("../../src/api/path")
-const { createMenuPath, deleteProductPath } = require('../helpers/pathFactory')
+
 const {BAD_REQUEST, OK, NOT_FOUND} = require("../../src/api/statusCode")
 const testObjects = require('../testObjects')
 
 const { bancheroRegistrationData, guerrinRegistrationData } = testObjects.pizzeriasRegistrationData
 const { mozzarella, bacon } = testObjects.productsData
+
+const {
+    registerPizzeria,
+    addProduct,
+    deleteProduct,
+    getMenu
+} = require('../helpers/apiHelperFunctions')
 
 describe('Api remove product from menu', () => {
     let requester
@@ -16,40 +22,34 @@ describe('Api remove product from menu', () => {
     })
 
     it(`given a registered pizzeria when a product is removed from it's menu the product is no longer on it's menu`, async () => {
-        await requester.post(registerPath).send(bancheroRegistrationData)
-        await requester.put(createMenuPath(bancheroRegistrationData.name)).send(mozzarella)
+        await registerPizzeria(requester, bancheroRegistrationData)
+        await addProduct(requester, bancheroRegistrationData, mozzarella)
 
-        await requester.delete(deleteProductPath(bancheroRegistrationData.name, mozzarella.name)).send()
+        await deleteProduct(requester, bancheroRegistrationData, mozzarella)
 
-        const getMenuResponse = await requester.get(createMenuPath(bancheroRegistrationData.name))
-        await expect(getMenuResponse.body).toEqual([])
+        const response = await getMenu(requester, bancheroRegistrationData)
+        await expect(response.body).toEqual([])
     })
 
     it(`given a registered pizzeria when a product is removed from it's menu the response has the removed product name`, async () => {
-        await requester.post(registerPath).send(bancheroRegistrationData)
-        await requester.put(createMenuPath(bancheroRegistrationData.name)).send(mozzarella)
+        await registerPizzeria(requester, bancheroRegistrationData)
+        await addProduct(requester, bancheroRegistrationData, mozzarella)
 
-        const response = await requester.delete(deleteProductPath(bancheroRegistrationData.name, mozzarella.name)).send()
+        const response = await deleteProduct(requester, bancheroRegistrationData, mozzarella)
 
         expect(response.status).toBe(OK)
         expect(response.body).toEqual({removed: mozzarella.name})
     })
 
     it(`given a registered pizzeria when it ask to remove a product missing from it's menu it fails`, async () => {
-        await requester.post(registerPath).send(bancheroRegistrationData)
-        await requester.put(createMenuPath(bancheroRegistrationData.name)).send(mozzarella)
+        await registerPizzeria(requester, bancheroRegistrationData)
+        await addProduct(requester, bancheroRegistrationData, mozzarella)
 
         const missingProductName = 'MISSING_PRODUCT_NAME'
-        const response = await requester.delete(deleteProductPath(bancheroRegistrationData.name, missingProductName)).send()
+        const response = await deleteProduct(requester, bancheroRegistrationData, {name: missingProductName})
 
         expect(response.status).toBe(NOT_FOUND)
         expect(response.body).toEqual({error: `Product ${missingProductName} not found` })
     })
 
 })
-
-async function sendRequestPutMenuCreation(requester, badPizza) {
-    return await requester
-        .put(createMenuPath(bancheroRegistrationData.name))
-        .send(badPizza)
-}
