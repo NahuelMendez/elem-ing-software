bcd5d35cf37003516b8aa3bd447ce80326a0bad7
Nahuel Mendez <mendez.nahuel@outlook.com>
2021-10-10 18:36:56 -0300

    Pizzeria info from api fixed

--- frontend/src/components/PizzeriaView/PizzeriaInfo.js
+++ frontend/src/components/PizzeriaView/PizzeriaInfo.js
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 import api from "../../Api/ApiObject";
 
 const PizzeriaInfo = ({ pizzeriaName }) => {
 
+    const [pizzeria, setPizzeria] = useState({ username: '', telephone: '', email: '' });
+
+    const getPizzeria = () => {
+        api.getPizzeria('pizzeria')
+        .then((response => {
+            setPizzeria(response.data);
+        }))
+        .catch( err => {
+            console.log(err)
+        });
+    }
+
+    useEffect(() => {
+        getPizzeria();
+        console.log("Pizzeria", pizzeria)
+    }, []);
+
     return (
-        <div></div>
+        <div className="piz-info-container">
+            <div className="pizz-info-image">
+            </div>
+            <div className="pizz-info-dtl">
+                <ul>
+                    <li>{pizzeria.username}</li>
+                    <li>{pizzeria.telephone}</li>
+                    <li>{pizzeria.email}</li>
+                </ul>
+            </div>
+        </div>
     );
 
 }
