476a123590dd8201cdd980f3d841989775a24d34
emilianaAilen <emiliana.ailen@hotmail.com>
2021-10-10 12:20:03 -0300

    search results with cards added

---
+++ frontend/src/components/UI/PizzeriaCard.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+import imgGeneric from "../../assets/generic-img.jpg"
+
+const PizzeriaCard = ({ name }) => {
+
+  return (
+    <Link style={{color: "black", textDecoration: "none"}} className="card w-1/6">
+      <div className="w-full">
+        <img src={imgGeneric} alt="" className="pizzeria-img"></img>
+      </div>
+      <div className="card-body">
+        <h2 className="card-tittle">{name}</h2>
+      </div>
+    </Link>
+  )
+}
+
+export default PizzeriaCard
