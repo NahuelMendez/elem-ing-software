d0682b1e2ca84238026b064b46c4e72b3ad887d5
emilianaAilen <emiliana.ailen@hotmail.com>
2021-10-10 11:04:16 -0300

    search initial functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import MyMenu from "./components/MyMenu";
 import MainView from "./components/MainView";
 import Profile from "./components/Profile";
 import RegisterView from "./components/Register";
+import SearchResult from "./components/SearchResult";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path="/login" component={Login}/>
       <Route path="/menu" component={MyMenu} />
       <Route path="/register" component={RegisterView}/>
+      <Route path="/busquedas" component={SearchResult}/>
     </Router>
   );
 }
