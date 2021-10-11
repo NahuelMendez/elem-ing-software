cc74a7c4225f6925d72a72c1f5fbdba5c0e70bcb
Nahuel Mendez <mendez.nahuel@outlook.com>
2021-10-10 21:06:08 -0300

    PizzeriaInfo working and basic styles added

--- frontend/src/components/PizzeriaView/PizzeriaInfo.js
+++ frontend/src/components/PizzeriaView/PizzeriaInfo.js
@@ -23,15 +23,19 @@ const PizzeriaInfo = ({ pizzeriaName }) => {
 
     return (
         <div className="piz-info-container">
-            <div className="pizz-info-image">
-                <img src={userIMG}></img>
-            </div>
-            <div className="pizz-info-dtl">
-                <ul>
-                    <li>{pizzeria.username}</li>
-                    <li>{pizzeria.telephone}</li>
-                    <li>{pizzeria.email}</li>
-                </ul>
+            <div className="piz-info-body-container">
+                <div className="piz-info-body">
+                    <div className="pizz-info-image">
+                        <img src={userIMG}></img>
+                    </div>
+                    <div className="pizz-info-dtl">
+                        <ul>
+                            <li>{pizzeria.username}</li>
+                            <li>{pizzeria.telephone}</li>
+                            <li>{pizzeria.email}</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     );
