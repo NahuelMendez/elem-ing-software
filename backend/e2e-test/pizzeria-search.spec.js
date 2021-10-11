b4fd1f4171b7073195475c2fbf7dc7518825b91f
David Eduardo Gonzalez <davidgonzalezjscosasunq@gmail.com>
2021-10-11 03:04:03 -0300

    e2e test - when a user enter a partial name that has some matches with pizzerias names, the page should have a card for each matching pizzeria containing its name and image

--- backend/e2e-test/pizzeria-search.spec.js
+++ backend/e2e-test/pizzeria-search.spec.js
@@ -54,4 +54,42 @@ describe('Pizzeria search by partial name', () => {
         await expectTextContent(page, "p", 'No se encontraron pizzerías que coincidan')
     })
 
+    it('when a user enter a partial name that has some matches with pizzerias names, the page should have a card for each matching pizzeria containing its name and image', async () => {
+        const kikoPizzaPizzeriaData = createPizzeriaRegistrationData({name: 'kiko pizza'})
+        const kentukyPizzeriaData = createPizzeriaRegistrationData({name: 'kentuky'})
+        const kePizzaPizzeriaData = createPizzeriaRegistrationData({name: 'ke pizza'})
+        await registerPizzeria(page, kikoPizzaPizzeriaData)
+        await registerPizzeria(page, kentukyPizzeriaData)
+        await registerPizzeria(page, kePizzaPizzeriaData)
+
+        const consumerData = createConsumerRegistrationData({})
+        await registerAndLoginConsumer(page, consumerData)
+
+        await page.type(searchInputSelector, 'ke')
+        await page.click(searchButtonSelector)
+
+        await page.waitForSelector('#root > div > .flex-wrap')
+
+        const result = await page.$$eval(
+            '#root > div > .flex-wrap > a',
+            elements => elements.map(
+                element => ({
+                    imageURL: element.querySelector('div > img.pizzeria-img').src,
+                    name: element.querySelector('.card-body').innerText
+                })
+            )
+        )
+
+        expect(result).toEqual([
+            {
+                imageURL: 'http://localhost:3000/static/media/generic-img.5eba155e.jpg',
+                name: 'kentuky'
+            },
+            {
+                imageURL: 'http://localhost:3000/static/media/generic-img.5eba155e.jpg',
+                name: 'ke pizza'
+            }
+        ])
+    })
+
 })
