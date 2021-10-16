4450dfc831a4e6b6f800d0401cce5820096e7553
David Eduardo Gonzalez <davidgonzalezjscosasunq@gmail.com>
2021-10-15 23:51:23 -0300

    replaced .pizzap-modal-content class on index.css for tailwind classes

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,6 +1,6 @@
 const Modal = ({title, body, handleClose}) =>
   <div className="pizzap-modal">
-    <div className="pizzap-modal-content">
+    <div className="rounded bg-white p-3">
       <header>
         <h3>{title} <span onClick={handleClose} name="modal-close-btn" className="float-right cursor-pointer text-3xl text-gray-400 hover:text-black">&times;</span></h3>
       </header>
