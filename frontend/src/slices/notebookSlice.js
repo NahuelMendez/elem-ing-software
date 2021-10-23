cb1191dad85d59831437aeff627df0f0e225f32f
emilianaAilen <emiliana.ailen@hotmail.com>
2021-10-23 15:09:02 -0300

    product parameter in filter

--- frontend/src/slices/notebookSlice.js
+++ frontend/src/slices/notebookSlice.js
@@ -8,23 +8,23 @@ export const slice = createSlice({
   reducers: {
     addProduct: (state, action) => {
       const  products = current(state).products
-      const productToAdd = products.find(i => i.name === action.payload.name)
+      const productToAdd = products.find(product => product.name === action.payload.name)
       if (productToAdd === undefined) {
         state.products = [...products, { ...action.payload, cant: 1 }]
       }
       else {
-        const productsClean = products.filter(i => i.name !== action.payload.name || i.pizzeriaName !== action.payload.pizzeriaName)
+        const productsClean = products.filter(product => product.name !== action.payload.name || product.pizzeriaName !== action.payload.pizzeriaName)
         state.products = [...productsClean, { ...action.payload, cant: productToAdd.cant + 1 }]
       }
     },
     removeProduct: (state, action) => {
       const  products = current(state).products
-      const productsClean = products.filter(i => i.name !== action.payload.name || i.pizzeriaName !== action.payload.pizzeriaName)
+      const productsClean = products.filter(product => product.name !== action.payload.name || product.pizzeriaName !== action.payload.pizzeriaName)
       state.products = productsClean
     },
     removeAllFromPizzeria: (state, action) => {
       const  products = current(state).products
-      const productsClean = products.filter(i => i.pizzeriaName !== action.payload)
+      const productsClean = products.filter(product => product.pizzeriaName !== action.payload)
       state.products = productsClean
     }
   }
