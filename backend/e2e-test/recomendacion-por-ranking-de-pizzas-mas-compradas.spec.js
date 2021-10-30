00378d9296b0c308f5491985c78b388b54f380d4
emilianaAilen <emiliana.ailen@hotmail.com>
2021-10-30 19:50:00 -0300

    test for ranking bestsellers added

---
+++ backend/e2e-test/recomendacion-por-ranking-de-pizzas-mas-compradas.spec.js
@@ -0,0 +1,78 @@
+const puppeteer = require('puppeteer')
+const {
+    goto,
+    registerAsPizzeriaAndGoToMenu,
+    registerPizzeriaWithAmountOfProducts,
+    addProduct,
+    expectTextContent,
+    registerAndLoginConsumer
+} = require('./helpers/helpers')
+
+const { createPizzeriaRegistrationData, createPizzaData, createConsumerRegistrationData, productsData } = require('../test/testObjects')
+
+jest.setTimeout(15000)
+
+describe('Consumidor - get ranking of most bought pizzas', () => {
+    let browser
+    let page
+
+    beforeEach(async () => {
+        browser = await puppeteer.launch({headless: false})
+        page = await browser.newPage()
+    })
+    
+    afterEach(async () => {
+        await browser.close()
+    })
+
+    it(`when there is at least one bought product in the system, a consumer should see a carousel in the main view`, async () => {
+        const { pizzeriaData } = await registerPizzeriaWithAmountOfProducts(page, 1)
+
+        await registerAndLoginConsumer(page, createConsumerRegistrationData({}))
+        await goto(page, `/pizzeria/${pizzeriaData.name}`)
+
+        await page.waitForSelector('.product-container .button-add > img')
+        await page.click('.product-container .button-add > img')
+       
+        await page.waitForSelector('[name="confirm-button"]')
+        await page.evaluate(() => document.querySelector('[name="confirm-button"]').click())
+
+        await goto(page, `/home`)
+
+        await page.waitForSelector('[name="carousel-ranking"]')
+    })
+
+    it(`each card in the carousel should show the name of the product, pizzeria name and its position in the ranking`, async () => {
+        const { pizzeriaData } = await registerPizzeriaWithAmountOfProducts(page, 1)
+
+        await registerAndLoginConsumer(page, createConsumerRegistrationData({}))
+        await goto(page, `/pizzeria/${pizzeriaData.name}`)
+
+        await page.waitForSelector('.product-container .button-add > img')
+        await page.click('.product-container .button-add > img')
+       
+        await page.waitForSelector('[name="confirm-button"]')
+        await page.evaluate(() => document.querySelector('[name="confirm-button"]').click())
+
+        await goto(page, `/home`)
+
+        await page.waitForSelector('[name="carousel-ranking"]')
+
+        await page.waitForSelector('[name="ranking-number"]')
+        await expectTextContent(page, '[name="ranking-number"]', "1")
+
+        await page.waitForSelector('[name="product-ranking-name"]')
+
+        await page.waitForSelector('[name="product-ranking-pizzeriaName"]')
+        await expectTextContent(page, '[name="product-ranking-pizzeriaName"]', pizzeriaData.name)
+    })
+
+    it('when there are no products sold, a consumer should not see the ranking carousel in the main view', async () => {
+        await registerAndLoginConsumer(page, createConsumerRegistrationData({}))
+
+        await goto(page, `/home`)
+
+        const foundElements = await page.$$('[name="carousel-ranking"]')
+        expect(foundElements).toHaveLength(0)
+    })
+})
--- frontend/src/components/Ranking/RankingCard.js
+++ frontend/src/components/Ranking/RankingCard.js
@@ -4,12 +4,12 @@ const RankingCard = ({ name, pizzeriaName, rankingNumber }) => {
     <div className="card mt-0 mr-0 w-2/3 h-64 product-container">
       <div className="flex justify-end">
         <div className="bg-principal mt-2 mr-2 rounded-full flex items-center justify-center w-10 h-10">
-          <p className="text-white text-xl font-bold m-0 p-0 text-center">{rankingNumber}</p>
+          <p name="ranking-number" className="text-white text-xl font-bold m-0 p-0 text-center">{rankingNumber}</p>
         </div>
       </div>
       <div className="card-body">
-        <h2 className="card-tittle">{name}</h2>
-        <p className="card-text text-secondary">{pizzeriaName}</p>
+        <h2 name="product-ranking-name" className="card-tittle">{name}</h2>
+        <p name="product-ranking-pizzeriaName" className="card-text text-secondary">{pizzeriaName}</p>
       </div>
     </div>
   )
