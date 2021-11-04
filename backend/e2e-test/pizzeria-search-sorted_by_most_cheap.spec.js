bf404b67b57edf70483e8708319347d4e47c40ad
davidgonzalezjscosasunq@gmail.com <davidgonzalezjscosasunq@gmail.com>
2021-11-04 12:27:48 -0300

    e2e - a consumer can choose to order pizzerias search results by most cheap acording to its average price with ascending order

--- backend/e2e-test/pizzeria-search-sorted_by_most_cheap.spec.js
+++ backend/e2e-test/pizzeria-search-sorted_by_most_cheap.spec.js
@@ -1,17 +1,19 @@
 const puppeteer = require('puppeteer')
+
 const {
+    registerPizzeria,
     registerPizzeriaWithAmountOfProducts,
     registerAndLoginConsumer,
     searchPizzerias,
-    clickAndWait,
     expectTextContent
 } = require('./helpers/helpers')
 
-const { createConsumerRegistrationData } = require('../test/testObjects')
+const { createConsumerRegistrationData, createPizzeriaRegistrationData } = require('../test/testObjects')
 
-jest.setTimeout(10000)
+jest.setTimeout(20000)
 
-const orderByMostCheapOptionSelector = 'select[name="orderBy"] option[value="MOST_CHEAP"]'
+const orderByDropdownSelector = 'select[name="orderBy"]'
+const orderByMostCheapOptionSelector = `${orderByDropdownSelector} option[value="MOST_CHEAP"]`
 
 describe('Pizzeria search by partial name sorted by most cheap', () => {
     let browser
@@ -27,14 +29,34 @@ describe('Pizzeria search by partial name sorted by most cheap', () => {
     })
 
     it('pizzerias search results page should have a dropdown with an option to sort pizzerias by most cheap', async () => {
-        const { pizzeriaData } = await registerPizzeriaWithAmountOfProducts(page, 1)
+        const pizzeriaName = `ZZZ${Date.now()}`
+        await registerPizzeria(page, createPizzeriaRegistrationData({name: pizzeriaName}))
   
         await registerAndLoginConsumer(page, createConsumerRegistrationData({}))
 
-        await searchPizzerias(page, pizzeriaData.name)
+        await searchPizzerias(page, pizzeriaName)
 
         await page.waitForSelector(orderByMostCheapOptionSelector)
         await expectTextContent(page, orderByMostCheapOptionSelector, 'Mas economica')
+    })
+    
+    it('a consumer can choose to order pizzerias search results by most cheap acording to its average price with ascending order', async () => {
+        const pizzeriaDataWith2Products = await registerPizzeriaWithAmountOfProducts(page, 2)
+        const pizzeriaDataWith1Products = await registerPizzeriaWithAmountOfProducts(page, 1)
+  
+        await registerAndLoginConsumer(page, createConsumerRegistrationData({}))
+
+        await searchPizzerias(page, 'p')
+
+        await page.click(orderByDropdownSelector)
+        await page.select(orderByDropdownSelector, "MOST_CHEAP")
+
+        await page.waitForTimeout(1000)
+        const cardTitles = await page.$$('.card-body')
+
+        expect(cardTitles).toHaveLength(2)
+        await expectTextContent(cardTitles[0], '.card-tittle', pizzeriaDataWith1Products.pizzeriaData.name) //TODO: corregir error ortografico en front y en tests ("tittle")
+        await expectTextContent(cardTitles[1], '.card-tittle', pizzeriaDataWith2Products.pizzeriaData.name)
     })
 
 })
--- frontend/src/components/SearchResult/index.js
+++ frontend/src/components/SearchResult/index.js
@@ -1,18 +1,45 @@
-import { useSelector } from "react-redux";
+import { useState } from 'react'
+import { useSelector, useDispatch } from "react-redux";
 import { searchState } from "../../slices/searchSlice";
+import { searchTextState, setSearchResults } from "../../slices/searchSlice";
 import NavBar from "../Navbar";
 import PizzeriaCard from "../UI/PizzeriaCard";
 
-const OrderByDropdown = () =>
-  <form>
-    <select name="orderBy">
-      <option value=""></option>
-      <option value="MOST_CHEAP">Mas economica</option>
-    </select>
-  </form>
+import api from "../../Api/ApiObject"
+
+const OrderByDropdown = ({handleChange}) => {
+  const [value, setValue] = useState('NAME')
+  
+  const localHandleChange = event => {
+    event.preventDefault()
+    setValue(event.target.value)
+    handleChange(event.target.value)
+  }
+  
+  return (
+    <form>
+      <label>Ordenar por:</label>
+      <select name="orderBy" value={value} onChange={localHandleChange}>
+        <option value="NAME"></option>
+        <option value="MOST_CHEAP">Mas economica</option>
+      </select>
+    </form>
+  )
+}
 
 const SearchResult = () => {
   const results = useSelector(searchState)
+  const searchText = useSelector(searchTextState)
+  const dispatch = useDispatch()
+
+  const handleOrderByChange = newOrderCriteria => {
+    api.searchPizzeria({ name: searchText, orderBy: newOrderCriteria }).then(res => {
+      dispatch(setSearchResults({
+        searchText,
+        results: res.data
+      }))
+    })
+  }
 
   return (
     <div className="w-full flex flex-col pb-8 items-center">
@@ -21,7 +48,7 @@ const SearchResult = () => {
       </div>
       <div className="w-5/6 flex flex-wrap justify-center">
         {results.length === 0 && <p className="text-lg mt-8 text-gray-500">No se encontraron pizzerías que coincidan.</p>}
-        {results && <OrderByDropdown />}
+        {results.length > 0 && <OrderByDropdown handleChange={handleOrderByChange}/>}
         {results && results.map((pizzeria, index) => (
           <PizzeriaCard key={index} name={pizzeria.name} />
         ))}
