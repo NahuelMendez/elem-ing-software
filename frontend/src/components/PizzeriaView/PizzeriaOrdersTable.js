7eb13f4e09452edb79c8ad583bcacaf9d821bf54
Nahuel Mendez <mendez.nahuel@outlook.com>
2021-11-06 04:42:39 -0300

    No orders yet message added

--- frontend/src/components/PizzeriaView/PizzeriaOrdersTable.js
+++ frontend/src/components/PizzeriaView/PizzeriaOrdersTable.js
@@ -1,39 +1,42 @@
-import Modal from "../Modal";
-import OrderDetail from "./OrderDetail";
 import OrderDetailButton from "./OrderDetailButton";
 import PizzeriaOrderDataRow from "./PizzeriaOrderDataRow";
 
 const PizzeriaOrdersTable = ({ orders }) => {
-    return (
-        <>
-            <PizzeriaOrderDataRow 
-                orderNum="Numero De Pedido"
-                customerName="Nombre Del Cliente"
-                telephone="Telefono"
-                email="Email"
-                total="TOTAL"
-                details="Detalles"
-                top={true}
-            />
-            {
-                orders.map(order => 
-                    <PizzeriaOrderDataRow 
-                        key={order.orderNumber}
-                        orderNum={order.orderNumber}
-                        customerName={order.consumerName}
-                        telephone={order.telephone}
-                        email={order.email}
-                        total={order.total}
-                        details={<OrderDetailButton
-                                    lineItems={order.lineItems}
-                                    total={order.total}
-                                />}
-                        top={false}
-                    />
-                )
-            }
-        </>
-    );
+
+    if (orders.length == 0) {
+        return ( <h2 className="text-center">Todavia no recibiste pedidos.</h2> );
+    } else {
+        return (
+            <>
+                <PizzeriaOrderDataRow 
+                    orderNum="Numero De Pedido"
+                    customerName="Nombre Del Cliente"
+                    telephone="Telefono"
+                    email="Email"
+                    total="TOTAL"
+                    details="Detalles"
+                    top={true}
+                />
+                {
+                    orders.map(order => 
+                        <PizzeriaOrderDataRow 
+                            key={order.orderNumber}
+                            orderNum={order.orderNumber}
+                            customerName={order.consumerName}
+                            telephone={order.telephone}
+                            email={order.email}
+                            total={order.total}
+                            details={<OrderDetailButton
+                                        lineItems={order.lineItems}
+                                        total={order.total}
+                                    />}
+                            top={false}
+                        />
+                    )
+                }
+            </>
+        );
+    }
 }
 
 export default PizzeriaOrdersTable;
