9af238c1f3a44cfc77cfb55d1e8a0f7c17271597
Nahuel Mendez <mendez.nahuel@outlook.com>
2021-11-14 02:06:31 -0300

    Added image for products in ranking

--- frontend/src/components/Ranking/RankingCard.js
+++ frontend/src/components/Ranking/RankingCard.js
@@ -1,8 +1,11 @@
-const RankingCard = ({ name, pizzeriaName, rankingNumber }) => {
+const RankingCard = ({ name, pizzeriaName, rankingNumber, pizzaImage }) => {
 
   return (
     <div className="card mt-0 mr-0 w-2/3 h-64 product-container ranking-card">
       <div className="flex justify-end">
+        <div className="ranking-image-container">
+          <img className="ranking-image" src={pizzaImage}></img>
+        </div>
         <div className="bg-principal mt-2 mr-2 rounded-full flex items-center justify-center w-10 h-10">
           <p name="ranking-number" className="text-white text-xl font-bold m-0 p-0 text-center">{rankingNumber}</p>
         </div>
